6f2253226686d8fa8e187fa773ea7fbacf33d1b2
wujian <3085038458@qq.com>
2021-09-18 00:50:33 +0800

    add sql

--- resources/m.sql
+++ resources/m.sql
@@ -6,3 +6,24 @@ create database ui;
 grant all privileges  on ui.* to 'uiserv'@'localhost';
 
 
+-- 组件库表 component
+-- id, key, name, desc
+create table if not exists `component` (
+    id int auto_increment,
+    name varchar(60) not null,
+    key varchar(60) not null,
+    version int,
+    type int, -- 组件是单组件，还是复合组件
+    primary key(`id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- 组件支持的属性表 property
+-- id, key, name, componentId
+create table if not exists `property` (
+    id int auto_increment,
+    key varchar(60) not null,
+    name varchar(60),
+    value varchar(100),
+    subjectKey varchar(50), -- 这个属性需要用什么描述
+    primary key(id)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
